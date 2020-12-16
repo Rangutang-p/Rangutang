0879daa0b38458163889d0bee5d044dcf560de21
Rangutang-p <73357721+Rangutang-p@users.noreply.github.com>
2020-12-16 09:17:39 -0500

    commit 2
    Organized files into folders, fixed files paths
    
    Added slideshow on landing page
    
    changed CSS to fit slideshow

--- Project 1/js/myscript.js
+++ Project 1/js/myscript.js
@@ -1,4 +1,4 @@
-/* function enabling collapsing menu */
+/* function enabling collapsing menu ----------------------------------------*/
   function expandmenu() {
     var x = document.getElementById("myTopnav")
     if (x.className === "main-nav") {
@@ -6,4 +6,41 @@
     } else {
       x.className = "main-nav";
     }
+    var c = document.getElementById("menuicon")
+
+    if (c.className === "fa fa-bars") {
+      c.className = " fa fa-times";
+    } else {
+      c.className = "fa fa-bars";
+    }
   }
+
+/* JS enabling slideshow ----------------------------------------------------*/
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// next/prev controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
